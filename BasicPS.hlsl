32fb6829238f7780e8aff677fd5b7f9b6d4530e8
syuuchicho <k021g1478@g.neec.ac.jp>
2022-07-27 12:09:57 +0900

    06_04シェーディング

--- BasicPS.hlsl
+++ BasicPS.hlsl
@@ -7,5 +7,17 @@ float4 main(VSOutput input) : SV_TARGET
 {
 	//return float4(1,1,1,1);
 	//return float4(tex.Sample(smp,input.uv))*color;
-	return float4(input.normal,1);//RGBをそれぞれ法線のXYZ,Aを1で出力
+	//return float4(input.normal,1);//RGBをそれぞれ法線のXYZ,Aを1で出力
+
+	//拡散反射光(diffuse)
+	//float3 light = normalize(float3(1,-1,1));					//右下奥　向きのライト
+	//float brightness = dot(-light,input.normal);				//光源へのベクトルと法線ベクトルの内積
+	//return float4(brightness, brightness, brightness, 1);		//輝度をRGBに代入して出力
+
+	//環境光(Ambient)
+	float3 light = normalize(float3(1,-1,-1));					//右下奥　向きのライト
+	float3 diffuse = saturate(dot(-light, input.normal));		//diffuseを[0,1]の範囲にClampする
+	float brightness = diffuse + 0.3f;							//アンビエント項を0.3として計算
+	float4 texcolor = float4(tex.Sample(smp, input.uv));
+	return float4(texcolor.rgb*brightness, texcolor.a)*color;		//輝度をRGBに代入して出力
 }
